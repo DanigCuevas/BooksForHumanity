1595e52d7803d8eb86dd0a9948b781ab23052b38
Daniela Cuevas <danic@MacBook-Pro-539.local>
2023-07-15 09:57:38 -0700

    Adding nav bar and about us

---
+++ src/components/About.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <>
+      <div className="container py-5">
+        <div className="row text-center text-black">
+          <div className="col-lg-8 mx-auto">
+            <h1 className="display-4">Our Team</h1>
+            <p className="lead mb-0">
+              Our team consists of 4 members passionate about creating a book sharing community.
+            </p>
+            <p className="lead">
+              Snippet by
+              <a href="https://bootstrapious.com/snippets" className="text-black">
+                <u>Bootstrapious</u>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row text-center justify-content-center">
+          <div className="col-md-6 col-lg-3 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4">
+              <img
+                src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg"
+                alt=""
+                width="100"
+                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+              />
+              <h5 className="mb-0">Team Member 1</h5>
+              <span className="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4">
+              <img
+                src="https://bootstrapious.com/i/snippets/sn-team/teacher-2.jpg"
+                alt=""
+                width="100"
+                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+              />
+              <h5 className="mb-0">Team Member 2</h5>
+              <span className="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4">
+              <img
+                src="https://bootstrapious.com/i/snippets/sn-team/teacher-1.jpg"
+                alt=""
+                width="100"
+                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+              />
+              <h5 className="mb-0">Team Member 3</h5>
+              <span className="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4">
+              <img
+                src="https://bootstrapious.com/i/snippets/sn-team/teacher-7.jpg"
+                alt=""
+                width="100"
+                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+              />
+              <h5 className="mb-0">Team Member 4</h5>
+              <span className="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+
