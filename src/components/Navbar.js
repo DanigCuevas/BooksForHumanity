59884ba4fbad491259963b2e259e8504f5c84d25
Maan Patel <maanpatel1811@gmail.com>
2023-07-16 09:57:40 -0500

    added the google book api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,60 @@
-import React from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    if (searchTerm) {
+      try {
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchTerm)}+intitle:${encodeURIComponent(searchTerm)}`);
+        const data = await response.json();
+        setSearchResults(data.items || []);
+      } catch (error) {
+        console.log('Error occurred while fetching data:', error);
+      }
+      setSearchTerm('');
+    }
+  };
+
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Eleventh navbar example">
-      <div className="container">
-        <Link to="/" className="navbar-brand">
-          <img src="assets/images/logo.jpg" alt="logo" class="navbarLogo" height="70">
- 
-          </img>
-        </Link>
-        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Eleventh navbar example">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            <img src="assets/images/logo.jpg" alt="logo" className="navbarLogo" height="70" />
+          </Link>
+          <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="navbar-collapse collapse" id="navbarsExample09" >
-          <form className="form-inline mx-auto w-50">
-            <div className="input-group">
-              <input
-                className="form-control"
-                type="search"
-                placeholder="Search for a book title"
-                aria-label="Search"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-dark" type="submit">
-                  Search
-                </button>
+          <div className="navbar-collapse collapse" id="navbarsExample09">
+            <form className="form-inline mx-auto w-50" onSubmit={handleSearch}>
+              <div className="input-group">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Search for a book title"
+                  aria-label="Search"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-dark" type="submit">
+                    Search
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
 
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
                 <Link to="/" className="nav-link">Discover</Link>
               </li>
-            <li className="nav-item">
+              <li className="nav-item">
                 <Link to="/about" className="nav-link">About Us</Link>
               </li>
               <li className="nav-item">
@@ -45,25 +63,32 @@ export default function Navbar() {
               <li className="nav-item">
                 <Link to="/cart" className="nav-link">Cart</Link>
               </li>
-             
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+
+      {searchResults.length > 0 && (
+        <div className="container">
+          <h2>Search Results</h2>
+          <div className="row">
+            {searchResults.map((book) => (
+              <div className="col-md-4" key={book.id}>
+                <div className="card">
+                  <img className="card-img-top" src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
+                  <div className="card-body">
+                    <h5 className="card-title">{book.volumeInfo.title}</h5>
+                    <p className="card-text">{book.volumeInfo.authors?.join(', ')}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
 
 
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  )
-}
